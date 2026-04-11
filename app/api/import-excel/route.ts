bc95b7ef97d0e58ee142bbeea937b9873987bed3
luvietnhan170697 <luvietnhan170697@gmail.com>
2026-04-11 09:38:26 +0700

    add import-excel l2

--- app/api/import-excel/route.ts
+++ app/api/import-excel/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import * as XLSX from "xlsx";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 export const dynamic = "force-dynamic";
 
